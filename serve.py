480d1719c1e1adbbf90c0cad62845bbe43b38252
Jesse Andrews <anotherjesse@gmail.com>
2024-07-08 16:12:27 -0700

    standardize on ports

--- serve.py
+++ serve.py
@@ -30,4 +30,4 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 
 if __name__ == "__main__":
-    uvicorn.run("serve:app", host="0.0.0.0", port=4000, reload=True, log_level="error")
+    uvicorn.run("serve:app", host="0.0.0.0", port=8000, reload=True, log_level="error")
